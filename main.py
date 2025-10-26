9ce9497b7b094815acc7f159b40e1b0ac2f3b7b9
Leighton Grant <oneal.grant@outlook.com>
2025-10-26 16:15:29 +0000

    feat(main): update .gitignore and add README2.md

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
+from os import path
+
+# with open('README2.md', 'w') as file_handler:
+#     try:
+#         file_handler.write('# TITLE')
+#     except Exception as e:
+#         print(f"An error occurred: {e}")
+#         raise e
+
+
+try:
+    with open('README3.md', 'r') as file_handler:
+        file_content = file_handler.read()
+        print(file_content)
+except Exception as e:
+    print(f"An error occurred: {e}")
+    # raise e
+
+# if path.exists('README2.md'):
 
 
 def main():
-    print("This is the main function")
+    print("")
 
 
 if __name__ == "__main__":
