4f3725e7d3101330c11da8367f65d59200f055b1
leightongrant <oneal.grant@outlook.com>
2025-10-30 15:53:23 +0000

    feat(questions): enhance prompts and improve formatting

--- questions.py
+++ questions.py
@@ -1,20 +1,47 @@
 from InquirerPy import inquirer
+from utilities import print_section_title
 
+print(
+    """
+Welcome ReadmeGenPY.
 
-title = inquirer.text(message="Project Title: ",
-                      default="Readme Generator").execute()
+Please follow the prompts to generate your readme.
 
+You can use markdown to add your installation steps on each line. Press Esc + Enter when done. Eg: 1. Create virtual environment `python3 -m venv .venv` 2. Install requirements 
+"""
+)
+
+print_section_title("About Your Project")
+title = inquirer.text(
+    message="What is the title of your project: ", default="Readme Generator"
+).execute()
 description = inquirer.text(
-    message="Project Description: ", default="A Python-based command-line tool to generate professional README.md files through a series of interactive prompts.").execute()
+    message="Project Description: ",
+    default="A Python-based command-line tool to generate professional README.md files through a series of interactive prompts.",
+).execute()
+
+print_section_title("How to install this project")
 
 installation = inquirer.text(
-    message="Enter installation instructions: ", multiline=True, default="* Install the interactive prompt dependency `pip3 install -r requirements.txt`").execute()
+    message="Enter installation instructions: ",
+    multiline=True,
+    default="* Install the interactive prompt dependency `pip3 install -r requirements.txt`",
+).execute()
+
+print_section_title("how to use this project")
+
+usage = inquirer.text(
+    message="Usage: ",
+    multiline=True,
+    default="Use this space to show useful examples of how a project can be used. Additional screenshots, code examples and demos work well in this space. You may also link to more resources.",
+)
+
+print_section_title("Choose a license")
 
 license = inquirer.select(
     message="Pick a License: ",
     choices=[
-        "MIT License"
-        "Apache License 2.0",
+        "MIT License" "Apache License 2.0",
         "GNU General Public License (GPL) v3",
         "Mozilla Public License 2.0",
         "BSD 3-Clause License",
@@ -22,12 +49,11 @@ license = inquirer.select(
     default="MIT License",
 ).execute()
 
-usage = inquirer.text(message="Usage: ", multiline=True,
-                      default="Use this space to show useful examples of how a project can be used. Additional screenshots, code examples and demos work well in this space. You may also link to more resources.")
 
-author = inquirer.text(message="Author Name: ").execute()
+print_section_title("contact information")
+
+author = inquirer.text(message="Author Name: ", default="Leighton Grant").execute()
 
 contact = inquirer.text(message="Contact Info : ", multiline=True).execute()
-
 
 confirm = inquirer.confirm(message="Confirm?").execute()
